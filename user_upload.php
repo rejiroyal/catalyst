80319093197549bb7ff66c1aac5eb4a2bbab66b9
rejiroyal <rejiroyal@gmail.com>
2021-09-08 16:30:56 +1000

    modified

--- user_upload.php
+++ user_upload.php
@@ -7,40 +7,40 @@ $db='UsersDB'; // Database name.
 
 //Connecting to MySQL database
 
-$con=mysqli_connect($host,$db_user,$db_password,$db);
+$con=mysqli_connect($host,$db_username,$db_password,$db);
 // Check connection
 if (mysqli_connect_errno())
 {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$query = "SELECT ID FROM users";
-$result = mysqli_query($con, $query);
-
-if(empty($result)) {
-                $query = "CREATE TABLE USERS (
-                          ID int(11) AUTO_INCREMENT,
-                          Firstname varchar(255) NOT NULL,
-                          Surname varchar(255) NOT NULL,
-                          Email varchar(255) NOT NULL UNIQUE,
-                          PRIMARY KEY  (ID)
-                          )";
-                $result = mysqli_query($dbConnection, $query);
-}
+  $query = "CREATE TABLE IF NOT EXISTS USERS (
+    ID int(11) AUTO_INCREMENT,
+    Firstname varchar(255) NOT NULL,
+    Surname varchar(255) NOT NULL,
+    Email varchar(255) NOT NULL UNIQUE,
+    PRIMARY KEY  (ID)
+    )";
+  if(mysqli_query($con, $query)){
+    echo "Table created successfully.";
+  } else{
+      echo "ERROR: Could not able to execute $query. " . mysqli_error($con);
+  }
+  mysqli_close($con);
  
- 
-$file = fopen("users.csv", "r");
-         while (($userData = fgetcsv($file, 10000, ",")) !== FALSE)
-         {
-          if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            print "Invalid email found, insert to user table has been skipped";
-          }
-           else{
-            $sql = "INSERT into tableName(name,email,address) values('$userData[0]','ucwords($userData[1])','strtolower($userData[2])')";
-            //ucwords - in-built php function - convert words to capitalize
-            //strtolower - in-built php function convert the words to lowercase
-            mysqli_query($con, $sql);
-           }   
-         }
-         fclose($file);
-         echo "CSV File has been successfully uploaded to users table.";
+  if (($handle      = fopen("users.csv", "r")) !== FALSE) {
+    while (($userData = fgetcsv($handle, 10000, ",")) !== FALSE)
+    {
+      if (!filter_var($userData[2], FILTER_VALIDATE_EMAIL)){
+      print "Invalid email found, insert to user table has been skipped";
+    }
+      else{
+        $sql = "INSERT into tableName(name,email,address) values('$userData[0]','ucwords($userData[1])','strtolower($userData[2])')";
+        //ucwords - in-built php function - convert words to capitalize
+        //strtolower - in-built php function convert the words to lowercase
+        mysqli_query($con, $sql);
+      }   
+    }
+  }  
+    echo "CSV File has been successfully uploaded to users table.";
+?>
